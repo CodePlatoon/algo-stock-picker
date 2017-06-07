1a04ec8076f214dd11b2efdbcb094de14ea41f04
Jonathan Young <jonyoungg@gmail.com>
2017-06-07 16:48:43 -0500

    Update stockPickerSpec.js

--- js/stockPickerSpec.js
+++ js/stockPickerSpec.js
@@ -1,6 +1,9 @@
+// run `npm install shallow-equal --save` in this folder before you get started with this challenge
 var sp = require("./stockPicker");
+var shallowEqualArrays = require('shallow-equal/arrays');
 
-console.log(sp.picker([17,3,6,9,15,8,6,1,10]) === [1,4]);
-console.log(sp.picker([3,17,6,9,18,15,6,1,10]) === [0,4]);
-console.log(sp.picker([1,17,6,9,8,15,6,3,19]) === [0,8]);
-console.log(sp.picker([19,17,6,9,8,15,6,3,1]) === [2,5]);
+console.log(shallowEqualArrays(sp.picker([17,3,6,9,15,8,6,1,10]) , [1, 4]))
+console.log(shallowEqualArrays(sp.picker([17,3,6,9,15,8,6,1,10]), [1,4]))
+console.log(shallowEqualArrays(sp.picker([3,17,6,9,18,15,6,1,10]), [0,4]))
+console.log(shallowEqualArrays(sp.picker([1,17,6,9,8,15,6,3,19]), [0,8]))
+console.log(shallowEqualArrays(sp.picker([19,17,6,9,8,15,6,3,1]), [2,5]))
